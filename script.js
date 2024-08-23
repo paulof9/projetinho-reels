4ccdbec684183af495df6ea6c05913d3f3f12422
paulof9 <above2@outlook.com>
2024-08-23 15:19:49 -0300

    Ajusta transição em window e transform, adiciona localStorage para o script ao fechar o window

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// popup
-const buttonX = document.querySelector('.fechar')
-const popup = document.querySelector('.window')
+document.addEventListener('DOMContentLoaded', function() {
+    const buttonX = document.querySelector('.fechar');
+    const popup = document.querySelector('.window');
 
-buttonX.addEventListener('click', function() {
-    popup.style = `
-        opacity: 0;
-        height: 0;
-        overflow: hidden;
-        visibility: hidden;
-        transition: all 2s;`;
-})
+    function hidePopup() {
+        popup.classList.add('hidden');
+        localStorage.setItem('popupClosed', 'true');
+
+        // display: none após a transição
+        setTimeout(() => {
+            popup.style.display = 'none';
+        }, 2000); // Tempo deve coincidir com a duração da transição
+    }
+
+    if (localStorage.getItem('popupClosed') === 'true') {
+        popup.classList.add('hidden');
+        popup.style.display = 'none'; // define
+    } else {
+        popup.classList.remove('hidden');
+        popup.style.display = 'flex';
+    }
+
+
+    buttonX.addEventListener('click', hidePopup);
+});
